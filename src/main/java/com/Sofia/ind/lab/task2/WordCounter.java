802a01492d86bf1ae05422f4b9a377ae6516b0f1
Gavry <lady.sonia@bk.ru>
2019-10-17 08:37:37 +0300

    task 2

--- src/main/java/com/Sofia/ind/lab/task2/WordCounter.java
+++ src/main/java/com/Sofia/ind/lab/task2/WordCounter.java
@@ -2,21 +2,34 @@ package com.Sofia.ind.lab.task2;
 
 public class WordCounter {
 
-    private static String strl = "a aasd";
-    private static char s ='a';
+    private static String strl = "a aasd bsd bsd";
     public static void main(String[] args) {
-       countChar(strl, s);
+        countChar(strl);
     }
-        public static int countChar(String str, char c)
+        public static void countChar (String str)
         {
-            int count = 0;
-
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == c)
-                    count++;
+            int i = 0, c = 0;
+            String[] str2 = str.split(" ");
+            for (String str3 : str2){
+                i++;
             }
-            System.out.println("Колличество вхождений - " + count);
-            return count;
+            for (int j=0; j< i-1; j++){
+                for (int k= j+1; k < i; k++){
+                    if (!str2[j].equals(" ")) {
+                        if (str2[j].equals(str2[k])){
+                            c++;
+                            str2[k]= " ";
+                        }
+                    }
+                }
+                if (!str2[j].equals(" "))
+                    System.out.println("количество вхождений " + str2[j] + " - " +(c+1));
+                c = 0;
+            }
+            if (!str2[i-1].equals(" "))
+                System.out.println("количество вхождений " + str2[i] + " - " + 1);
+
         }
+
 
 }
