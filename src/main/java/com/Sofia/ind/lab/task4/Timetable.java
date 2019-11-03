754e4bbb1853439221758d8844dbdfe299882d0b
Gavry <lady.sonia@bk.ru>
2019-11-03 22:19:47 +0300

    task 2

--- src/main/java/com/Sofia/ind/lab/task4/Timetable.java
+++ src/main/java/com/Sofia/ind/lab/task4/Timetable.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 
         public class Timetable {
         private Group[] groups;
-        private String lesson;
+        private Teacher[] teachers;
         private String time;
         private String day;
 
-    public Timetable(Group[] group, String lesson, String time, String day){
+    public Timetable(Group[] group, Teacher[] teacher, String time, String day){
         this.groups = group;
-        this.lesson = lesson;
+        this.teachers = teacher;
         this.time = time;
         this.day = day;
     }
@@ -23,14 +23,24 @@ import java.util.Scanner;
         groups[groups.length] = group;
     }
 
-    @Override
+    public void addTeacher(Teacher teacher){
+        teachers = Arrays.copyOf(teachers, teachers.length + 1);
+        teachers[teachers.length] = teacher;
+    }
 
-            public String toString(){
-                return String.format("%n"+"Day: " + day +
-                        "%n"+"Lesson: " +
-                        lesson + "%n"+"Time: " +
+
+    @Override
+    public String toString(){
+        return Arrays.toString(teachers);
+    }
+            public String toString1(){
+                return String.format("%n"+"Teacher: "+teachers+
+                        "%n"+"Day: " + day +
+                        "%n"+"Time: " +
                         time +"%n");
             }
+
+
 
     public static void main(String[] args) {
 
@@ -42,20 +52,18 @@ import java.util.Scanner;
         Group g1 = new Group( new Student[] {S1, S2});//создали группу, в которую поместили студента
         Group g2 = new Group( new Student[] {S3, S4});
 
-        Timetable T1 = new Timetable(new Group[]{g1},"Java","11:30","Monday");
-        Timetable T2 = new Timetable(new Group[]{g1},"C++","13:10", "Monday");
+        Teacher E1 = new Teacher("Maluhin","App Math");
+        Teacher E2 = new Teacher ("Sinica","Web");
 
-        Timetable T3 = new Timetable(new Group[]{g1},"C++","11:30", "Monday");
-        Timetable T4 = new Timetable(new Group[]{g1},"Oracle","13:10","Monday");
+        Timetable T1 = new Timetable(new Group[]{g1},new Teacher[]{E1},"11:30","Monday");
+        Timetable T2 = new Timetable(new Group[]{g1},new Teacher[]{E2},"13:10", "Monday");
 
+        Timetable T3 = new Timetable(new Group[]{g2},new Teacher[]{E2},"11:30", "Monday");
+        Timetable T4 = new Timetable(new Group[]{g2},new Teacher[]{E1},"13:10","Monday");
 
 
         System.out.printf("Student:" + S1 + "%n");
         System.out.printf(T1 +"%n" +
                 T2);
     }
-
-
-
-
 }
