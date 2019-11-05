0d94e1d5a16c100134221327420d3fa5aee19da2
Gavry <lady.sonia@bk.ru>
2019-11-05 12:08:27 +0300

    task 2

--- src/main/java/com/Sofia/ind/lab/task4/Group.java
+++ src/main/java/com/Sofia/ind/lab/task4/Group.java
@@ -16,21 +16,4 @@ public class Group {
         students[students.length] = student;//в последнюю позицию записываем нового студента
     }
 
-    @Override
-    public String toString(){
-        return Arrays.toString(students);
-    }
-    public static void main(String[] args) {
-
-        /*
-        Student S1 = new Student("Sarah", "App math", 21, 32);
-        Student S2 = new Student("Thomas", "App math", 20, 32);
-
-        Group g1 = new Group( new Student[] {S1, S2});//создали группу, в которую поместили студента
-
-        System.out.println(g1);
-        */
-    }
-
-
 }
