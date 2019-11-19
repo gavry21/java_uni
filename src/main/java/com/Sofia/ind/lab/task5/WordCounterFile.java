58e67fa1f395cda83f6955e97d6c4ec979f5abd7
Gavry <lady.sonia@bk.ru>
2019-11-19 11:48:08 +0300

    task 5

--- src/main/java/com/Sofia/ind/lab/task5/WordCounterFile.java
+++ src/main/java/com/Sofia/ind/lab/task5/WordCounterFile.java
@@ -2,33 +2,28 @@ package com.Sofia.ind.lab.task5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class WordCounterFile {
-    public static void main(String[] args) throws FileNotFoundException {
-
-        getWordCount("C:\\Users\\Sofia\\Documents\\words.txt");
-    }
-
-    public static void getWordCount  (String filename)throws FileNotFoundException
-    {
-        int c = 0;
-        File text = new File(filename);
-        Scanner scanner = new Scanner(text);
-        String[] words = scanner.split(" ");
-        for(String word : words){
-            c = find(words,word);
-            System.out.println("Количество вхождений"+word+": "+find(words,word));
-        }
-    }
-
-    private static int find(String[] words, String findWord) {
-        int count=0;
-        for(String word : words){
-            if (word.equals(findWord)) {
+    public static void WordsCounter(String fileName, Map<String, Integer> words) throws FileNotFoundException {
+        Scanner file = new Scanner(new File(fileName));
+        while (file.hasNext()){
+            String word = file.next();
+            Integer count = words.get(word);
+            if(count != null)
                 count++;
-            }
+            else
+                count =1;
+            words.put(word, count);
         }
-        return count;
+        file.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Integer> words = new HashMap<String, Integer>();
+        WordsCounter("C:\\Users\\Sofia\\Documents\\words.txt", words);
+        System.out.println(words);
     }
 }
