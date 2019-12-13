19b457aa14eac011245bfcf897bb64b696fe7a54
Gavry <lady.sonia@bk.ru>
2019-12-13 19:57:09 +0300

    group chat again

---
+++ src/main/java/com/Sofia/ind/lab/task6/Server.java
@@ -0,0 +1,118 @@
+package com.Sofia.ind.lab.task6;
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
+
+class ServerSomthing extends Thread {
+
+    private Socket socket; // сокет, через который сервер общается с клиентом,
+    // кроме него - клиент и сервер никак не связаны
+    private BufferedReader in; // поток чтения из сокета
+    private BufferedWriter out; // поток завписи в сокет
+
+     // для общения с клиентом необходим сокет (адресные данные)
+
+
+    public ServerSomthing(Socket socket) throws IOException {
+        this.socket = socket;
+        // если потоку ввода/вывода приведут к генерированию искдючения, оно проброситься дальше
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        // сооюбщений новому поключению
+        start(); // вызываем run()
+    }
+    @Override
+    public void run() {
+        String word;
+        try {
+            // первое сообщение отправленное сюда - это никнейм
+            word = in.readLine();
+            try {
+                out.write(word + "\n");
+                out.flush(); // flush() нужен для выталкивания оставшихся данных
+                // если такие есть, и очистки потока для дьнейших нужд
+            } catch (IOException ignored) {}
+            try {
+                while (true) {
+                    word = in.readLine();
+                    if(word.equals("stop")) {
+                        this.downService(); // харакири
+                        break; // если пришла пустая строка - выходим из цикла прослушки
+                    }
+                    System.out.println(word);
+
+                    for (ServerSomthing vr : Server.serverList) {
+                        vr.send(word); // отослать принятое сообщение с привязанного клиента всем остальным влючая его
+                    }
+                }
+            } catch (NullPointerException ignored) {}
+
+
+        } catch (IOException e) {
+            this.downService();
+        }
+    }
+
+
+     //отсылка одного сообщения клиенту по указанному потоку
+
+
+    private void send(String msg) {
+        try {
+            out.write(msg + "\n");
+            out.flush();
+        } catch (IOException ignored) {}
+
+    }
+
+    /**
+     * закрытие сервера
+     * прерывание себя как нити и удаление из списка нитей
+     */
+    private void downService() {
+        try {
+            if(!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+                for (ServerSomthing vr : Server.serverList) {
+                    if(vr.equals(this)) vr.interrupt();
+                    Server.serverList.remove(this);
+                }
+            }
+        } catch (IOException ignored) {}
+    }
+}
+
+
+
+
+public class Server {
+
+    public static final int PORT = 8000;
+    public static LinkedList<ServerSomthing> serverList = new LinkedList<>(); // список всех нитей - экземпляров
+    // сервера, слушающих каждый своего клиента
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(PORT);
+
+        System.out.println("Server Started");
+        try {
+            while (true) {
+                // Блокируется до возникновения нового соединения:
+                Socket socket = server.accept();
+                try {
+                    serverList.add(new ServerSomthing(socket)); // добавить новое соединенние в список
+                } catch (IOException e) {
+                    // Если завершится неудачей, закрывается сокет,
+                    // в противном случае, нить закроет его:
+                    socket.close();
+                }
+            }
+        } finally {
+            server.close();
+        }
+    }
+
+}
